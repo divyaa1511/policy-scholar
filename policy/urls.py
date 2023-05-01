c8d60b274b1e5fa7103d74702172dc3990041e08
divyaa1511 <102688183+divyaa1511@users.noreply.github.com>
2023-05-01 23:21:16 +0530

    Update urls.py

--- policy/urls.py
+++ policy/urls.py
@@ -21,7 +21,7 @@ urlpatterns=[
 
     path('post/new',PostCreateView.as_view(),name='post-new'),
 
-path('post/<int:pk>/',views.PostDetail,name='post-detail'),
+    path('post/<str:title>/',views.PostDetail,name='post-detail'),
     path('post/<int:pk>/update',PostUpdateView.as_view(),name='post-update'),
 
     path('post/<int:pk>/delete',PostDeleteView.as_view(),name='post-delete')
